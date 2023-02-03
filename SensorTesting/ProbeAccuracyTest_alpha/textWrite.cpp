5fe0164fa0f34bbe5251bc94d42541bd6053735d
Brandon <103676674+BrandonGarnica@users.noreply.github.com>
2023-02-03 14:28:27 -0700

    Adhere to coding standard

--- SensorTesting/ProbeAccuracyTest_alpha/textWrite.cpp
+++ SensorTesting/ProbeAccuracyTest_alpha/textWrite.cpp
@@ -1,17 +1,24 @@
-#include "textWrite.h"
-#include "calcSteps.h"
 #include <Arduino.h>
 
-void calcData(int stepData[9]) { // We should really get rid of this, seems like we can just call writeSerialMonitor();
-  writeSerialMonitor(&stepData[0]);
+#include "textWrite.h"
+#include "calcSteps.h"
+
+// Variable Defines
+#define NUM_OF_TESTS 10
+#define NUM_OF_DATA NUM_OF_TESTS - 1
+#define INIT_VAR 0
+#define SIG_FIGS 4
+
+void calcData(int stepData[NUM_OF_DATA]) { // We should really get rid of this, seems like we can just call writeSerialMonitor();
+  writeSerialMonitor(&stepData[INIT_VAR]);
 };
 
 // Write data to serial monitor
-void writeSerialMonitor(int stepData[9]) {
-  for(int i = 0; i < 10; i++) {
+void writeSerialMonitor(int stepData[NUM_OF_DATA]) {
+  for(int i = INIT_VAR; i < NUM_OF_TESTS; i++) {
     Serial.print(stepData[i]);
     Serial.print(" (");
-    Serial.print(calcDistance(stepData[i]),4);
+    Serial.print(calcDistance(stepData[i]), SIG_FIGS);
     Serial.print("mm)");
     Serial.print("\n");
   }
