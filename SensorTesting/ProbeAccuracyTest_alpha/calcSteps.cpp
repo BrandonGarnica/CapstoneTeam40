4dc856d78366d71c178c09b29b1f29ca2d3fd4cb
Brandon <103676674+BrandonGarnica@users.noreply.github.com>
2023-02-03 12:04:40 -0700

    Update Define & for-loop declaration

--- SensorTesting/ProbeAccuracyTest_alpha/calcSteps.cpp
+++ SensorTesting/ProbeAccuracyTest_alpha/calcSteps.cpp
@@ -7,11 +7,11 @@
 
 #define MOTOR_STEPS 200
 #define ONE_REV_8MM 8
-#define ONE_STEP_40UM 40e-6
+#define ONE_STEP_40MM 40e-3
 
 // Calculate the distance movement from the amount of steps taken
 double calcDistance(int stepsTaken) {
-  return stepsTaken * ONE_STEP_40UM;  // 1 step is 40um
+  return stepsTaken * ONE_STEP_40MM;  // 1 step is 40um
 }
 
 // Calculate & return the average from the data array
@@ -19,12 +19,10 @@ double calcAvg(double data[]) {
 
   double sum;
   int sizeOfArray = sizeof(data)/sizeof(data[0]); // Get the size of the array
-
-  // Start @ pos 1, because pos 0 has init position
-  for (int i = 1; i < sizeOfArray; i++) {
+  
+  for (int i = 0; i < sizeOfArray; i++) {
     sum += data[i];
   }
-
   return sum / sizeOfArray;
 }
 
