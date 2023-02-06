ccbd2134356e24eff69fc9fd9a70099afc9ddad2
Brandon <103676674+BrandonGarnica@users.noreply.github.com>
2023-02-06 08:50:31 -0700

    Remove unused functions
    Removed unused functions for calculating step data

--- SensorTesting/ProbeAccuracyTest_alpha/calcSteps.cpp
+++ SensorTesting/ProbeAccuracyTest_alpha/calcSteps.cpp
@@ -13,19 +13,3 @@
 double calcDistance(int stepsTaken) {
   return stepsTaken * ONE_STEP_40MM;  // 1 step is 40um
 }
-
-// Calculate & return the average from the data array
-double calcAvg(double data[]) {
-
-  double sum;
-  int sizeOfArray = sizeof(data)/sizeof(data[0]); // Get the size of the array
-  
-  for (int i = 0; i < sizeOfArray; i++) {
-    sum += data[i];
-  }
-  return sum / sizeOfArray;
-}
-
-// Calculate & return the range from the data array
-double calcRange(double data[]) {
-}
--- SensorTesting/ProbeAccuracyTest_alpha/calcSteps.h
+++ SensorTesting/ProbeAccuracyTest_alpha/calcSteps.h
@@ -7,10 +7,4 @@
 // Calculate the distance movement from the amount of steps taken
 double calcDistance (int stepsTaken);
 
-// Calculate & return the average from the data array
-double calcAvg (double data[]);
-
-// Calculate & return the range from the data array
-double calcRange (double data[]);
-
 #endif /* CALCSTEPS_H */
