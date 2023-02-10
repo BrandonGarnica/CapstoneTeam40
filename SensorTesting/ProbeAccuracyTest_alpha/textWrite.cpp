345177d935907e13849b818077e89d7db97d5da0
Brandon <103676674+BrandonGarnica@users.noreply.github.com>
2023-02-10 08:43:52 -0700

    Remove Redundant Function

--- SensorTesting/ProbeAccuracyTest_alpha/textWrite.cpp
+++ SensorTesting/ProbeAccuracyTest_alpha/textWrite.cpp
@@ -9,10 +9,6 @@
 #define INIT_VAR 0
 #define SIG_FIGS 4
 
-void calcData(int stepData[NUM_OF_DATA]) { // We should really get rid of this, seems like we can just call writeSerialMonitor();
-  writeSerialMonitor(&stepData[INIT_VAR]);
-};
-
 // Write data to serial monitor
 void writeSerialMonitor(int stepData[NUM_OF_DATA]) {
   for(int i = INIT_VAR; i < NUM_OF_TESTS; i++) {
@@ -20,7 +16,7 @@ void writeSerialMonitor(int stepData[NUM_OF_DATA]) {
     //Serial.print(" (");
     //Serial.print(calcDistance(stepData[i]), SIG_FIGS);
     //Serial.print("mm)");
-    //Serial.print("\n");
+    Serial.print("\n");
   }
   Serial.print("\n");
   return;
--- SensorTesting/ProbeAccuracyTest_alpha/textWrite.h
+++ SensorTesting/ProbeAccuracyTest_alpha/textWrite.h
@@ -1,9 +1,6 @@
 #ifndef PROBEACCURACYTEST_ALPHA_TEXTWRITE_H
 #define PROBEACCURACYTEST_ALPHA_TEXTWRITE_H
 
-// Calculate data
-void calcData(int stepData[9]);
-
 // Write data to serial monitor
 void writeSerialMonitor(int stepData[9]);
 
