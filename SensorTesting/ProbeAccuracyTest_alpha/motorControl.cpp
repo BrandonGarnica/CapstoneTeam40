ed0a07445ff2158ef01288eab1c75bd2046b47bf
Brandon <103676674+BrandonGarnica@users.noreply.github.com>
2023-04-07 09:10:05 -0600

    Remove unnecessary define + clean up SM
    Removed unnecessary defines and cleaned up SM for better performance.

--- SensorTesting/ProbeAccuracyTest_alpha/motorControl.cpp
+++ SensorTesting/ProbeAccuracyTest_alpha/motorControl.cpp
@@ -34,18 +34,26 @@
 #define MOVE_BACK_X HALF_TURN
 
 // Variable Defines
-#define NUM_OF_TESTS 10
-#define NUM_OF_DATA NUM_OF_TESTS
-#define TRIGGER_VAL 2.5
+#define NUM_OF_TESTS 9
+#define NUM_OF_SAMPLES NUM_OF_TESTS + 1
+#define NUM_0F_STEPS_SAMPLES 1600
 #define RESET 0
 #define STEP_DATA_POS 0
 #define DELAY_DEFAULT 250
 #define DELAY_POST_PROBE 10
 #define DELAY_INIT_PROBING 25
+#define DELAY_FWD_PROBING 100
 #define DELAY_PROBING 250
-#define MIN_PROBE_DISTANCE 100
-#define ANALOG_VOLT_100MM 0.681
-#define VOLT_DIF 0.025
+#define PROBE_OFFSET_DIST MOVE_ONE_MM * 50
+
+#define LOW_VOLT 0.074
+#define HIGH_VOLT 4.464
+#define LOW_ANALOG 14.000
+#define HIGH_ANALOG 975.000
+#define ANALOG_VOLT_80mm 0.375
+#define ANALOG_VOLT_MIN 0.1
+#define ANALOG_VOLT_HIGH ANALOG_VOLT_MIN
+#define VOLT_DIF 0.005
 
 // SM Declaration
 enum motor_States {
@@ -60,26 +68,30 @@ enum motor_States {
 // Stepper Declaration
 BasicStepperDriver stepper(MOTOR_STEPS, PIN_DIR, PIN_STEP);
 
-// Variables
-bool firstProbe = true;           // Variable to track if its the first probe of the test
-bool isAnalog = true;             // Var for if input is analog or discrete
+int stepDataResults [NUM_OF_SAMPLES] = {};
+int inputVoltage    [NUM_OF_SAMPLES] = {};
 
-int stepsTakenFWD;                // Steps taken FWD by motor
-int stepsTakenBKWD;               // Steps taken BKWD by motor
-int sensorLowPosition;            // X
-int sensorHighPosition;
-int dataEntry;                    // Keeps track of data entry #
-int stepData [NUM_OF_DATA] = {};  // Init data point based on the number of tests
+int numOfSamples;
+int numOfTests;
+int initSteps;                    // Tracks the init steps back until sensor is HIGH
+int backupSteps;                  // Tracks the # of steps to back up until sensor is low
+int offsetSteps;                  // Tracks the offset of step need to reposition probe back to init state
 
-int initSteps;
-int backupSteps;
-int offsetSteps;
+float mapRange;                   // Map input analog to voltage range
 
-float range;
-float lowVolt = 0.063;
-float highVolt = 4.457;
-float lowAnalog = 20.000;
-float highAnalog = 970.0;
+// Returns button state
+bool motorControl_buttonState() { return digitalRead(PIN_BTN); }
+
+// Returns probe state
+// bool motorControl_probeState() { return digitalRead(PIN_PROBE); } // Probe is set as NO (HIGH = 5V, LOW = 0V)
+bool motorControl_probeState() { return (motorControl_mapInput(analogRead(0)) >= ANALOG_VOLT_HIGH && motorControl_mapInput(analogRead(0)) <= ANALOG_VOLT_HIGH + VOLT_DIF)? 1:0; } // Probe returns high when analog is above min
+
+// Helper function for moving stepper motor
+void motorControl_moveMotor(int direction, int numOfSteps, int rpm = RPM_MOVING, int microsteps = MICROSTEPS, int delayMS = DELAY_DEFAULT) {
+  stepper.begin(rpm, microsteps);
+  stepper.move(direction*numOfSteps*microsteps);
+  delay(delayMS);
+}
 
 // Inits SM & Variables used
 void motorControl_init() { 
@@ -90,9 +102,9 @@ void motorControl_init() {
   // Init PINS on arduino
   pinMode(PIN_PROBE, INPUT);
   pinMode(PIN_BTN, INPUT_PULLUP);
-    
+
   // Init ultrasonic sensor range
-  range = (highVolt - lowVolt) / (highAnalog - lowAnalog);
+  mapRange = (HIGH_VOLT - LOW_VOLT) / (HIGH_ANALOG - LOW_ANALOG);  
   
   // Init SM
   current_State = init_st;
@@ -100,21 +112,7 @@ void motorControl_init() {
 
 // Maps the input analog to a voltage output
 float motorControl_mapInput(float analogInput) {
-  return (analogInput - lowAnalog) * range + lowVolt;
-}
-
-// Returns button state
-bool motorControl_buttonState() { return digitalRead(PIN_BTN); }
-
-// Returns probe state
-// bool motorControl_probeState() { return digitalRead(PIN_PROBE); } // Probe is set as NO (HIGH = 5V, LOW = 0V)
-bool motorControl_probeState() { return (motorControl_mapInput(analogRead(0)) >= ANALOG_VOLT_100MM && motorControl_mapInput(analogRead(0)) <= ANALOG_VOLT_100MM + VOLT_DIF)? 1:0; } // Probe returns high when analog is above min
-
-// Helper function for moving stepper motor
-void motorControl_moveMotor(int direction, int numOfSteps, int rpm = RPM_MOVING, int microsteps = MICROSTEPS, int delayMS = DELAY_DEFAULT) {
-  stepper.begin(rpm, microsteps);
-  stepper.move(direction*numOfSteps*microsteps);
-  delay(delayMS);
+  return (analogInput - LOW_ANALOG) * mapRange + LOW_VOLT;
 }
 
 // Motor Control tick function
@@ -147,41 +145,53 @@ void motorControl_tick() {
     case backup_st:
       // Move motor back
       backupSteps += MOVE_BACK_X;
-      motorControl_moveMotor(BKWD, MOVE_BACK_X, RPM_MOVING, MICROSTEPS, DELAY_PROBING);
+      motorControl_moveMotor(BKWD, MOVE_BACK_X, RPM_PROBING, MICROSTEPS, DELAY_PROBING);
       if(!motorControl_probeState()){
         current_State = forward_st;
       }
       break;
 
     case forward_st:
-      if(dataEntry >= NUM_OF_TESTS) {
+      if(numOfSamples >= NUM_OF_SAMPLES) {
+        delay(100);
+        inputVoltage[numOfSamples] = analogRead(0);
         // Store data
-        stepData[dataEntry] = (backupSteps - offsetSteps) + initSteps;
+        stepDataResults[numOfSamples] = (backupSteps - offsetSteps) + initSteps + PROBE_OFFSET_DIST;
         // Move stepper back to offset
         motorControl_moveMotor(FWD, (backupSteps - offsetSteps), RPM_PROBING, MICROSTEPS, DELAY_POST_PROBE);
         // Move to init position
-        motorControl_moveMotor(FWD, initSteps, RPM_MOVING, MICROSTEPS, DELAY_POST_PROBE);
+        motorControl_moveMotor(FWD, initSteps, RPM_MOVING, MICROSTEPS, DELAY_PROBING);
         current_State = write_st;
       } else {
         if(motorControl_probeState()){
-          stepData[dataEntry++] = (backupSteps - offsetSteps) + initSteps;
+          delay(100);
+          inputVoltage[numOfSamples] = analogRead(0);
+          stepDataResults[numOfSamples++] = (backupSteps - offsetSteps) + initSteps + PROBE_OFFSET_DIST;
           // Adjust offset
-          motorControl_moveMotor(FWD, (backupSteps - offsetSteps), RPM_PROBING, MICROSTEPS, DELAY_POST_PROBE);
+          motorControl_moveMotor(FWD, (backupSteps - offsetSteps), RPM_PROBING, MICROSTEPS, DELAY_PROBING);
           offsetSteps = RESET;
           backupSteps = RESET;
           current_State = backup_st;
         } else {
           offsetSteps += MOVE_BY_X;
           // Increment motor forward
-          motorControl_moveMotor(FWD, MOVE_BY_X, RPM_MOVING, MICROSTEPS, DELAY_PROBING);
+          motorControl_moveMotor(FWD, MOVE_BY_X, RPM_MOVING, MICROSTEPS, DELAY_FWD_PROBING);
         }
       }
       break;
 
     case write_st:
+      Serial.print("Test #");
+      Serial.println(numOfTests + 1);
       // Write data to serial monitor
-      writeSerialMonitor(stepData);
-      current_State = init_st;
+      writeSerialMonitor(stepDataResults);
+      writeSerialMonitor(inputVoltage);
+      if(numOfTests >= NUM_OF_TESTS) {
+        current_State = init_st;
+      } else {
+        numOfTests++;
+        current_State = init_probe_st;
+      }
       break;
 
     default:
@@ -192,15 +202,17 @@ void motorControl_tick() {
 
     case init_st:
       // Reset variables
-      dataEntry = RESET;
-      stepsTakenBKWD = RESET;
-      sensorHighPosition = RESET;
-      sensorLowPosition = RESET;
+      numOfSamples = RESET;
+      numOfTests = RESET;
       initSteps = RESET;
       offsetSteps = RESET;
       backupSteps = RESET;
       break;
       
+    case init_probe_st:
+      // Might need to reset arrays
+      break;
+
     case wait_st:
       // Do nothing...
       break;
@@ -214,6 +226,10 @@ void motorControl_tick() {
       break;
 
     case write_st:
+      numOfSamples = RESET;
+      initSteps = RESET;
+      offsetSteps = RESET;
+      backupSteps = RESET;
       // Do nothing...
       break;
 
